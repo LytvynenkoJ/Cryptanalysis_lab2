72740aaea53931ab6644070fae6d89596561b68a
LytvynenkoJ <70521634+LytvynenkoJ@users.noreply.github.com>
2022-11-06 14:47:03 +0400

    Опрацьовуємо текст, рахуємо к-сть кожного символа
    Позбавляємося від великих літер та спецсимволів в оригінальному тексті
    Паралельно з цим рахуємо кількість входжень кожного з символів алфавіту

--- Lytvynenko lab2.py
+++ Lytvynenko lab2.py
@@ -12,3 +12,20 @@ file.close()
 alphabetS="абвгдеєжзиіїйклмнопрстуфхцчшщьюя"
 alphabetB="АБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
 alphabetS_list = list(alphabetS)
+
+#опрацьовуємо текст: позбавляємось від символів, які не входять до алфавіту
+#   та замінюємо великі літери на малі
+#одночасно з цим підраховуємо кількість кожного з символів алфавіту
+freq=[0 for j in range(32)]
+coun=[0 for j in range(32)]
+file = open("text2.txt", "w", encoding="utf8")
+for i in range(len(text)):
+    fS = alphabetS.find(text[i])
+    fB = alphabetB.find(text[i])
+    if fS!=-1:
+        coun[fS]+=1
+        file.write(alphabetS[fS])
+    if fB!=-1:
+        coun[fS]+=1
+        file.write(alphabetS[fB])
+file.close()
